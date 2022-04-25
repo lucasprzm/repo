310dc824798bf56a7f71b22436197dbb3cad83c8
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-25 16:53:44 -0300

    início do projeto

---
+++ src/Routes.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes as RoutesWrapper } from "react-router-dom";
+import Main from "./pages/Main";
+import Repositorio from "./pages/Repositorio";
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <RoutesWrapper>
+        <Route path="/" element={<Main />} />
+        <Route path="/repositorio/:repositorio" element={<Repositorio />} />
+      </RoutesWrapper>
+    </BrowserRouter>
+  );
+}
