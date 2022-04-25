361ca123068167d5a0ffaabb7043f18e405c265a
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-25 17:25:32 -0300

    estilização página main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,19 @@
+import { Container, Form, SubmitButton } from "./styles";
+import { FaGithub, FaPlus } from "react-icons/fa";
+
 export default function Main() {
   return (
-    <div>
-      <h1>Main</h1>
-    </div>
+    <Container>
+      <h1>
+        <FaGithub size={25} />
+        Meus Repositórios
+      </h1>
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar Repositório" />
+        <SubmitButton>
+          <FaPlus color="#fff" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
