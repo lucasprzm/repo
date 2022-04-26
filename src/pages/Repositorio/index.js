1e9084e726dd542c4516f25f2c555af99c523562
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-26 14:03:54 -0300

    estilização e criação de issues

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Container, Owner, Loading, BackButton } from "./styles";
+import { Container, Owner, Loading, BackButton, IssuesList } from "./styles";
 import { instance } from "../../services/api";
 import { FaArrowLeft } from "react-icons/fa";
 
@@ -46,6 +46,24 @@ export default function Repositorio() {
         <h1>{repositorio.name}</h1>
         <p>{repositorio.description}</p>
       </Owner>
+      <IssuesList>
+        {issues.map((issue) => {
+          return (
+            <li key={String(issue.id)}>
+              <img src={issue.user.avatar_url} alt={issue.user.login} />
+              <div>
+                <strong>
+                  <a href={issue.html_url}>{issue.title}</a>
+                  {issue.labels.map((label) => {
+                    return <span key={String(label.id)}>{label.name}</span>;
+                  })}
+                </strong>
+                <p>{issue.user.login}</p>
+              </div>
+            </li>
+          );
+        })}
+      </IssuesList>
     </Container>
   );
 }
