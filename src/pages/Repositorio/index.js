bb7499827cdf665cda1c31981b9626b25437b1af
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-26 12:27:14 -0300

    estilização e estruturação página repositórios

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,10 +1,51 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { Container, Owner, Loading, BackButton } from "./styles";
+import { instance } from "../../services/api";
+import { FaArrowLeft } from "react-icons/fa";
 
 export default function Repositorio() {
+  const [repositorio, setRepositorio] = useState();
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   let params = useParams();
+  useEffect(() => {
+    async function load() {
+      const nomeRepo = decodeURIComponent(params.repositorio);
+
+      const [repositorioData, issuesData] = await Promise.all([
+        instance.get(`/repos/${nomeRepo}`),
+        instance.get(`/repos/${nomeRepo}/issues`, {
+          params: {
+            state: "open",
+            per_page: 5,
+          },
+        }),
+      ]);
+      setRepositorio(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+    load();
+  }, [params.repositorio]);
+  if (loading) {
+    return (
+      <Loading>
+        <h1>Carregando...</h1>
+      </Loading>
+    );
+  }
   return (
-    <div>
-      <h1 style={{ color: "#fff" }}>{decodeURIComponent(params.repositorio)}</h1>
-    </div>
+    <Container>
+      <BackButton to="/">
+        <FaArrowLeft color="#000" size={30} />
+      </BackButton>
+      <Owner>
+        <img src={repositorio.owner.avatar_url} alt={repositorio.owner.login} />
+        <h1>{repositorio.name}</h1>
+        <p>{repositorio.description}</p>
+      </Owner>
+    </Container>
   );
 }
